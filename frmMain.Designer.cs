43eeeddc57de9b4ed021ad14aebddbda5981b56b
Mohammad Mehrnia <qermezkon@gmail.com>
2021-01-19 19:44:53 +0330

    -

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBoxVoice = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBoxLine = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,31 +39,10 @@
             this.btnConf = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "صدای متن به گفتار:";
-            // 
-            // comboBoxVoice
-            // 
-            this.comboBoxVoice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBoxVoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxVoice.FormattingEnabled = true;
-            this.comboBoxVoice.Location = new System.Drawing.Point(134, 16);
-            this.comboBoxVoice.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.comboBoxVoice.Name = "comboBoxVoice";
-            this.comboBoxVoice.Size = new System.Drawing.Size(732, 24);
-            this.comboBoxVoice.TabIndex = 1;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Location = new System.Drawing.Point(14, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(107, 16);
             this.label2.TabIndex = 2;
@@ -77,7 +54,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxLine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxLine.FormattingEnabled = true;
-            this.comboBoxLine.Location = new System.Drawing.Point(134, 67);
+            this.comboBoxLine.Location = new System.Drawing.Point(134, 13);
             this.comboBoxLine.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBoxLine.Name = "comboBoxLine";
             this.comboBoxLine.Size = new System.Drawing.Size(732, 24);
@@ -86,7 +63,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Location = new System.Drawing.Point(36, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 16);
             this.label3.TabIndex = 4;
@@ -96,7 +73,7 @@
             // 
             this.textBoxNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxNumber.Location = new System.Drawing.Point(134, 116);
+            this.textBoxNumber.Location = new System.Drawing.Point(134, 52);
             this.textBoxNumber.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBoxNumber.Name = "textBoxNumber";
             this.textBoxNumber.Size = new System.Drawing.Size(618, 23);
@@ -106,7 +83,7 @@
             // 
             this.buttonDial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonDial.AutoSize = true;
-            this.buttonDial.Location = new System.Drawing.Point(759, 113);
+            this.buttonDial.Location = new System.Drawing.Point(760, 45);
             this.buttonDial.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonDial.Name = "buttonDial";
             this.buttonDial.Size = new System.Drawing.Size(106, 36);
@@ -126,17 +103,17 @@
             this.listBoxLog.Items.AddRange(new object[] {
             "برای برقراری تماس خروجی یک خط را انتخاب کنید ، یک شماره را وارد کنید و روی شماره " +
                 "گیری کلیک کنید."});
-            this.listBoxLog.Location = new System.Drawing.Point(17, 199);
+            this.listBoxLog.Location = new System.Drawing.Point(17, 135);
             this.listBoxLog.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBoxLog.Name = "listBoxLog";
-            this.listBoxLog.Size = new System.Drawing.Size(850, 276);
+            this.listBoxLog.Size = new System.Drawing.Size(850, 340);
             this.listBoxLog.TabIndex = 7;
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(134, 160);
+            this.textBox1.Location = new System.Drawing.Point(136, 96);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(618, 23);
@@ -145,7 +122,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 169);
+            this.label4.Location = new System.Drawing.Point(36, 99);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 16);
             this.label4.TabIndex = 9;
@@ -155,7 +132,7 @@
             // 
             this.btnConf.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnConf.AutoSize = true;
-            this.btnConf.Location = new System.Drawing.Point(758, 155);
+            this.btnConf.Location = new System.Drawing.Point(760, 89);
             this.btnConf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnConf.Name = "btnConf";
             this.btnConf.Size = new System.Drawing.Size(106, 36);
@@ -178,8 +155,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBoxLine);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBoxVoice);
-            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmMain";
@@ -193,9 +168,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBoxVoice;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBoxLine;
         private System.Windows.Forms.Label label3;
