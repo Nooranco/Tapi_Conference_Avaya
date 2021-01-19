d8c1cc2e741910fd49933d204803e1da3cdf6dd3
Mohammad Mehrnia <qermezkon@gmail.com>
2021-01-19 19:41:29 +0330

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace OutgoingCalls
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxVoice = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxLine = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.buttonDial = new System.Windows.Forms.Button();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "صدای متن به گفتار:";
+            // 
+            // comboBoxVoice
+            // 
+            this.comboBoxVoice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxVoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxVoice.FormattingEnabled = true;
+            this.comboBoxVoice.Location = new System.Drawing.Point(134, 16);
+            this.comboBoxVoice.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxVoice.Name = "comboBoxVoice";
+            this.comboBoxVoice.Size = new System.Drawing.Size(732, 24);
+            this.comboBoxVoice.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "خط مورد استفاده:";
+            // 
+            // comboBoxLine
+            // 
+            this.comboBoxLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxLine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLine.FormattingEnabled = true;
+            this.comboBoxLine.Location = new System.Drawing.Point(134, 67);
+            this.comboBoxLine.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxLine.Name = "comboBoxLine";
+            this.comboBoxLine.Size = new System.Drawing.Size(732, 24);
+            this.comboBoxLine.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "شماره تماس:";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxNumber.Location = new System.Drawing.Point(134, 116);
+            this.textBoxNumber.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(618, 23);
+            this.textBoxNumber.TabIndex = 5;
+            // 
+            // buttonDial
+            // 
+            this.buttonDial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDial.AutoSize = true;
+            this.buttonDial.Location = new System.Drawing.Point(759, 113);
+            this.buttonDial.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonDial.Name = "buttonDial";
+            this.buttonDial.Size = new System.Drawing.Size(106, 36);
+            this.buttonDial.TabIndex = 6;
+            this.buttonDial.Text = "شماره گیری";
+            this.buttonDial.UseVisualStyleBackColor = true;
+            this.buttonDial.Click += new System.EventHandler(this.buttonDial_Click);
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.HorizontalScrollbar = true;
+            this.listBoxLog.ItemHeight = 16;
+            this.listBoxLog.Items.AddRange(new object[] {
+            "برای برقراری تماس خروجی یک خط را انتخاب کنید ، یک شماره را وارد کنید و روی شماره " +
+                "گیری کلیک کنید."});
+            this.listBoxLog.Location = new System.Drawing.Point(17, 199);
+            this.listBoxLog.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(850, 276);
+            this.listBoxLog.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(134, 160);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(618, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "شماره تماس:";
+            // 
+            // btnConf
+            // 
+            this.btnConf.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConf.AutoSize = true;
+            this.btnConf.Location = new System.Drawing.Point(758, 155);
+            this.btnConf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnConf.Name = "btnConf";
+            this.btnConf.Size = new System.Drawing.Size(106, 36);
+            this.btnConf.TabIndex = 10;
+            this.btnConf.Text = "کنفرانس";
+            this.btnConf.UseVisualStyleBackColor = true;
+            this.btnConf.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 491);
+            this.Controls.Add(this.btnConf);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.buttonDial);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxLine);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxVoice);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "تمای های گرفته شده";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxVoice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxLine;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Button buttonDial;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConf;
+    }
+}
+
